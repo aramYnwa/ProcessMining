d39e0a1272d78cb423eccb50a3fbcec61e6feed0
Aram <aram@ut.ee>
2018-04-04 16:58:14 +0300

    Added AssosiationBased classifer for set based classifications

---
+++ XES/src/com/company/classifiers/AssosiationBasedClassifier.java
@@ -0,0 +1,35 @@
+package com.company.classifiers;
+
+import com.company.xlog.XLogHandler;
+import java.util.List;
+
+public class AssosiationBasedClassifier extends ArffLogClassifier{
+
+  public AssosiationBasedClassifier(XLogHandler handler) {
+    super(handler);
+  }
+
+  @Override
+  protected void createAttributes (List<String> file) {
+
+    String relationName = "@RELATION " + logHandler.getFileName();
+    file.add(relationName);
+    file.add("\n");
+
+    for (String attribute : logHandler.getAttributes()) {
+      String arffAttr = "@ATTRIBUTE " + attribute + " {0, 1}";
+      file.add(arffAttr);
+    }
+
+    // Add the label column which is binary
+    // but in ARFF format there is no BINARY type.
+    String labelAttribute = "@ATTRIBUTE class {0, 1}";
+    file.add(labelAttribute);
+    file.add("\n");
+  }
+
+  @Override
+  protected void fillValue(List<Integer> instance, int index) {
+      instance.set(index, 1);
+  }
+}
